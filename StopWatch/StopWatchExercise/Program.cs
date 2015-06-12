dc3036111c250685c7a08ad69659e6b879ca3c6f
aarguijo89 <aarguijo89@gmail.com>
2015-06-11 20:01:30 -0600

    StopWatch Exercise
    First commit

---
+++ StopWatch/StopWatchExercise/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+
+namespace StopWatchExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var stopWatch = new StopWatch();
+
+                stopWatch.Start();
+                Thread.Sleep(8);
+                stopWatch.Stop();
+
+                Console.WriteLine("The duration is: {0}", stopWatch.GetDuration());
+
+                stopWatch.Start();
+                Thread.Sleep(20);
+                stopWatch.Stop();
+
+                Console.WriteLine("The duration is: {0}", stopWatch.GetDuration());
+            }
+            catch (InvalidOperationException exception)
+            {
+                Console.WriteLine("Error: {0}", exception.Message);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Oops an unexpected error occurred:  {0}",exception.Message);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ StopWatch/StopWatchExercise/StopWatch.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StopWatchExercise
+{
+    public class StopWatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+        private bool _isStarted;
+
+
+        public void Start()
+        {
+            if (_isStarted)
+                throw new InvalidOperationException("The StopWatch is already started");
+
+            _isStarted = true;
+            _startTime = DateTime.Now;
+        }
+
+        public void Stop()
+        {
+            if (!(_isStarted))
+                throw new InvalidOperationException("StopWatch is not started");
+
+            _isStarted = false;
+            _endTime = DateTime.Now;
+        }
+
+        public TimeSpan GetDuration()
+        {
+            if (_isStarted)
+                throw new InvalidOperationException("StopWatch have not been stopped");
+
+            return _endTime - _startTime;
+        }
+
+    }
+}
