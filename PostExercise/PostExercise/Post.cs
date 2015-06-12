6e994728a86d6e550cce71b482c07f671de9758d
aarguijo89 <aarguijo89@gmail.com>
2015-06-11 21:03:05 -0600

    First commit
    Exercise 2: design a StackOverFlow Post from Udemy Course

---
+++ PostExercise/PostExercise/Post.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PostExercise
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; private set; }
+        private int _voteCount;
+
+        public Post()
+        {
+            CreatedDate = DateTime.Now;
+        }
+
+        public Post(string title, string description):this()
+        {
+            Title = title;
+            Description = description;
+        }
+
+        public void UpVote()
+        {
+            _voteCount ++;
+        }
+
+        public void DownVote()
+        {
+            _voteCount--;
+        }
+
+        public int GetVoteCount()
+        {
+            return _voteCount;
+        }
+
+    }
+}
---
+++ PostExercise/PostExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PostExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var post = new Post("HellO!","Here is a description!!!");
+            post.UpVote();
+            post.UpVote();
+            post.DownVote();
+            post.UpVote();
+            Console.WriteLine("Votes: {0}", post.GetVoteCount());
+
+            Console.ReadLine();
+        }
+    }
+}
